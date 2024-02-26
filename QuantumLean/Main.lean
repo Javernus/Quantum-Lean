faa5ff741ae0a37708675658f1bd840c92fcbf6f
Jake Jongejans <jake.jongejans@enreach.com>
2024-02-26 09:43:40 +0100

    feat: Create rudimentary proof of Deutsch Algorithm.
    TO DO: Rework into better proof with future knowledge.

--- QuantumLean/Main.lean
+++ QuantumLean/Main.lean
@@ -43,3 +43,54 @@ theorem HXHeqZ : Hadamard * XGate * Hadamard = 2 * ZGate :=
     simp [mul_apply]
     rw [ofNat_fin_two]
     norm_num
+
+-- Create the Oracle for the Deutsch Algorithm, i.e. O(a, b) = !![-1^a, 0; 0, 1^b] where a, b ∈ {0, 1}
+def Oracle (a b : Bool) : Matrix (Fin 2) (Fin 2) ℤ :=
+  !![(-1)^(a.toNat), 0; 0, (-1)^(b.toNat)]
+
+@[simp] lemma Oracle_apply_01 (a b : Bool) : (Oracle a b) 0 1 = 0 := by simp [Oracle]
+@[simp] lemma Oracle_apply_10 (a b : Bool) : (Oracle a b) 1 0 = 0 := by simp [Oracle]
+
+@[simp] lemma Oracle_apply_00 (a b : Bool) : (Oracle a b) 0 0 = (-1)^(a.toNat) := by simp [Oracle]
+@[simp] lemma Oracle_apply_11 (a b : Bool) : (Oracle a b) 1 1 = (-1)^(b.toNat) := by simp [Oracle]
+
+def ZeroQbit : Matrix (Fin 1) (Fin 2) ℤ := !![1, 0]
+def OneQbit : Matrix (Fin 1) (Fin 2) ℤ := !![0, 1]
+def PlusQbit : Matrix (Fin 1) (Fin 2) ℤ := !![1, 1]
+def MinusQbit : Matrix (Fin 1) (Fin 2) ℤ := !![1, -1]
+
+def Measure (q : Matrix (Fin 1) (Fin 2) ℤ) : Bool :=
+  (q 0 0) = 0
+
+-- The Deutsch Algorithm
+theorem DeutschAlgorithm (a b : Bool) : Measure (PlusQbit * (Oracle a b) * Hadamard) = (a != b) :=
+  by
+    -- Following solves the equality for each case: write it once
+    -- simp [Oracle, PlusQbit, Hadamard, mul_apply]
+    -- norm_num
+    cases a; cases b;
+    simp [Measure, Oracle, PlusQbit, Hadamard, mul_apply]
+    simp [Measure, Oracle, PlusQbit, Hadamard, mul_apply]
+    cases b;
+    simp [Measure, Oracle, PlusQbit, Hadamard, mul_apply]
+    simp [Measure, Oracle, PlusQbit, Hadamard, mul_apply]
+
+theorem DeutschNew (a b : Bool) : Hadamard * (Oracle a b) * Hadamard = 2 * (-1)^(a.toNat) * XGate^(a.toNat + b.toNat) :=
+  by
+    cases a; cases b;
+    simp [XGate, Oracle, PlusQbit, Hadamard, mul_apply]
+    rw [ofNat_fin_two]
+    norm_num
+    simp [XGate, Oracle, PlusQbit, Hadamard, mul_apply]
+    rw [ofNat_fin_two]
+    norm_num
+    cases b;
+    simp [XGate, Oracle, PlusQbit, Hadamard, mul_apply]
+    rw [ofNat_fin_two]
+    norm_num
+    simp [XGate, Oracle, PlusQbit, Hadamard, mul_apply]
+    rw [ofNat_fin_two]
+    norm_num
+
+
+-- Create a theorem with inputs a and b which are either 0 or 1 (Natural number type)
