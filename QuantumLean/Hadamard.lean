690a32c4a6bc56526f90c049f1729733243d4a20
Jake Jongejans <jake.jongejans@enreach.com>
2024-04-05 14:59:37 +0200

    feat: 2^n Hadamard squared = Identity proven.

--- QuantumLean/Hadamard.lean
+++ QuantumLean/Hadamard.lean
@@ -1,13 +1,104 @@
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Matrix.Notation
+import Mathlib.Data.Matrix.Kronecker
 
 open Matrix
+open Kronecker
 
-def Hadamard : Matrix (Fin 2) (Fin 2) ℤ :=
-  !![1, 1; 1, -1]
 
-@[simp] lemma Hadamard_apply (j : Fin 2) : Hadamard j 0 = 1 := by simp [Hadamard]
+section HnxHn
+variable { n : ℕ }
 
-@[simp] lemma Hadamard_apply11 : Hadamard 1 1 = -1 := by simp [Hadamard]
+@[simp]
+def reindexHadamard (A : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ) : Matrix (Fin (2 ^ (n + 1))) (Fin (2 ^ (n + 1))) ℤ :=
+  reindex finProdFinEquiv finProdFinEquiv A
 
-@[simp] lemma Hadamard_apply01 : Hadamard 0 1 = 1 := by simp [Hadamard]
+
+@[simp]
+theorem reindexHadamard_smul (A : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ) (c : ℕ) : reindexHadamard (c • A) = c • reindexHadamard A := by
+  simp only [reindexHadamard, Nat.add_eq, Nat.add_zero, Nat.pow_eq, reindex_apply]
+  rw [submatrix_smul]
+  simp
+
+@[simp]
+theorem reindexHadamard_one : reindexHadamard (1 : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ) = 1 := by
+  simp
+
+
+@[simp]
+theorem reindexHadamard_smul_one (c : ℕ) : reindexHadamard (c • (1 : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ)) = c • 1 := by
+  rw [reindexHadamard_smul, reindexHadamard_one]
+
+
+@[simp]
+def Hadamard : (n : ℕ) -> Matrix (Fin (2 ^ n)) (Fin (2 ^ n)) ℤ
+  | 0 => 1
+  | (n + 1) => reindexHadamard (Hadamard n ⊗ₖ !![1, 1; 1, -1])
+
+@[simp]
+theorem HeqH : Hadamard 1 = !![1, 1; 1, -1] := by decide
+
+
+@[simp]
+theorem HxH : Hadamard 1 * Hadamard 1 = 2 := by decide
+
+
+@[simp]
+theorem TwoXI : (2 : Matrix (Fin 2) (Fin 2) ℤ) = (2 : ℕ) • 1 := by simp
+
+
+@[simp]
+theorem TwoToNXI : (2 ^ n : Matrix (Fin (2 ^ n)) (Fin (2 ^ n)) ℤ) = (2 ^ n : ℕ) • 1 := by simp
+
+
+@[simp]
+theorem TwoToNPlus1XI : (2 ^ (n + 1) : Matrix (Fin (2 ^ (n + 1))) (Fin (2 ^ (n + 1))) ℤ) = (2 ^ (n + 1) : ℕ) • 1 := by
+  simp only [smul_eq_mul, mul_comm, pow_succ]
+  rw [mul_comm]
+  simp
+
+
+@[simp]
+theorem TwoXTwoToNXI : (2 : ℕ) • (2 ^ n : ℕ) = 2 ^ (n + 1) := by
+  rw [smul_eq_mul, mul_comm, ← pow_succ]
+
+
+@[simp]
+theorem Hsuccn : Hadamard (n + 1) = reindexHadamard (Hadamard n ⊗ₖ !![1, 1; 1, -1]) := by rfl
+
+
+
+@[simp]
+theorem postKroneckerReindex : reindexHadamard (M ⊗ₖ !![1, 1; 1, -1]) * reindexHadamard (M ⊗ₖ !![1, 1; 1, -1]) = reindexHadamard ((M ⊗ₖ !![1, 1; 1, -1]) * (M ⊗ₖ !![1, 1; 1, -1])) := by
+  simp
+
+
+-- In general, I am trying to prove H * H = I (× a constant) for Hadamard matrices
+theorem HnxHn : Hadamard n * Hadamard n = 2 ^ n * 1 := by
+  induction n with
+    | zero => simp
+    | succ n ih =>
+      rw [Hsuccn]
+      simp only [Nat.succ_eq_add_one]
+      -- Turn current (Hn ⊗ₖ H1) * (Hn ⊗ₖ H1) into (Hn * Hn) ⊗ₖ (H1 * H1)
+      rw [postKroneckerReindex, ← mul_kronecker_mul, ← HeqH, HxH, ih]
+      simp only [mul_one]
+      rw [TwoXI, TwoToNXI, kroneckerMap_smul_right _ (2 : ℕ) _ (2 ^ n • 1) 1, kroneckerMap_smul_left _ (2 ^ n) _ _ 1]
+      rw [kroneckerMap_one_one]
+      -- Why do I now have 4 cases to prove? I thought I was only rewriting equivalences...
+      case succ.hf₁ => simp
+      case succ.hf₂ => simp
+      case succ.hf₃ => simp
+      case succ =>
+        -- I don't really know how to solve this anymore, in my head, this is just an equality
+        -- but with the reindexHadamard and powers, I am struggling.
+        rw [← smul_assoc, TwoXTwoToNXI, TwoToNPlus1XI, reindexHadamard_smul_one]
+      simp [nsmul_eq_mul, Nat.cast_pow, Nat.cast_ofNat, mul_assoc]
+      simp [nsmul_eq_mul, mul_comm, mul_assoc]
+
+end HnxHn
+
+
+-- Mathlib Data Finset Prod
+-- Cartesian Product? In type def of Hadamard for size of matrix
+-- Theorem that proves ⊗ₖ indices can be reindexed without any change to the matrix
--- QuantumLean/Main.lean
+++ QuantumLean/Main.lean
@@ -1,3 +1,5 @@
+-- import Mathlib.Tactic.LibrarySearch
+
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Matrix.Notation
 import Mathlib.Data.Complex.Basic
@@ -5,92 +7,57 @@ import Mathlib.Data.Complex.Basic
 -- Import the gates
 import «QuantumLean».Hadamard
 import «QuantumLean».PauliGates
-import «QuantumLean».NecessaryCode
 
 open Matrix
 open Complex
 
 
-theorem HH : Hadamard * Hadamard = 2 :=
-  by
-    rw [Hadamard]
-    simp [mul_apply]
-    rw [ofNat_fin_two]
-    norm_num
+theorem XX : XGate * XGate = 1 := by decide
+theorem ZZ : ZGate * ZGate = 1 := by decide
 
-theorem XX : XGate * XGate = 1 :=
-  by
-    rw [XGate]
-    simp [mul_apply]
-    rw [one_fin_two]
-
-theorem ZZ : ZGate * ZGate = 1 :=
-  by
-    rw [ZGate]
-    simp [mul_apply]
-    rw [one_fin_two]
-
-theorem HZHeqX : Hadamard * ZGate * Hadamard = 2 * XGate :=
-  by
-    rw [Hadamard, XGate, ZGate]
-    simp [mul_apply]
-    rw [ofNat_fin_two]
-    norm_num
-
-theorem HXHeqZ : Hadamard * XGate * Hadamard = 2 * ZGate :=
-  by
-    rw [Hadamard, XGate, ZGate]
-    simp [mul_apply]
-    rw [ofNat_fin_two]
-    norm_num
+theorem HZHeqX : Hadamard 1 * ZGate * Hadamard 1 = 2 * XGate := by decide
+theorem HXHeqZ : Hadamard 1 * XGate * Hadamard 1 = 2 * ZGate := by decide
 
 -- Create the Oracle for the Deutsch Algorithm, i.e. O(a, b) = !![-1^a, 0; 0, 1^b] where a, b ∈ {0, 1}
+@[simp]
 def Oracle (a b : Bool) : Matrix (Fin 2) (Fin 2) ℤ :=
   !![(-1)^(a.toNat), 0; 0, (-1)^(b.toNat)]
 
-@[simp] lemma Oracle_apply_01 (a b : Bool) : (Oracle a b) 0 1 = 0 := by simp [Oracle]
-@[simp] lemma Oracle_apply_10 (a b : Bool) : (Oracle a b) 1 0 = 0 := by simp [Oracle]
 
-@[simp] lemma Oracle_apply_00 (a b : Bool) : (Oracle a b) 0 0 = (-1)^(a.toNat) := by simp [Oracle]
-@[simp] lemma Oracle_apply_11 (a b : Bool) : (Oracle a b) 1 1 = (-1)^(b.toNat) := by simp [Oracle]
-
-def ZeroQbit : Matrix (Fin 1) (Fin 2) ℤ := !![1, 0]
-def OneQbit : Matrix (Fin 1) (Fin 2) ℤ := !![0, 1]
+@[simp]
 def PlusQbit : Matrix (Fin 1) (Fin 2) ℤ := !![1, 1]
-def MinusQbit : Matrix (Fin 1) (Fin 2) ℤ := !![1, -1]
 
+
+@[simp]
 def Measure (q : Matrix (Fin 1) (Fin 2) ℤ) : Bool :=
   (q 0 0) = 0
 
 -- The Deutsch Algorithm
-theorem DeutschAlgorithm (a b : Bool) : Measure (PlusQbit * (Oracle a b) * Hadamard) = (a != b) :=
+theorem DeutschAlgorithm (a b : Bool) : Measure (PlusQbit * (Oracle a b) * Hadamard 1) = (a != b) :=
   by
-    -- Following solves the equality for each case: write it once
-    -- simp [Oracle, PlusQbit, Hadamard, mul_apply]
-    -- norm_num
     cases a; cases b;
-    simp [Measure, Oracle, PlusQbit, Hadamard, mul_apply]
-    simp [Measure, Oracle, PlusQbit, Hadamard, mul_apply]
+    decide
+    decide
     cases b;
-    simp [Measure, Oracle, PlusQbit, Hadamard, mul_apply]
-    simp [Measure, Oracle, PlusQbit, Hadamard, mul_apply]
+    decide
+    decide
 
-theorem DeutschNew (a b : Bool) : Hadamard * (Oracle a b) * Hadamard = 2 * (-1)^(a.toNat) * XGate^(a.toNat + b.toNat) :=
-  by
-    cases a; cases b;
-    simp [XGate, Oracle, PlusQbit, Hadamard, mul_apply]
-    rw [ofNat_fin_two]
-    norm_num
-    simp [XGate, Oracle, PlusQbit, Hadamard, mul_apply]
-    rw [ofNat_fin_two]
-    norm_num
-    cases b;
-    simp [XGate, Oracle, PlusQbit, Hadamard, mul_apply]
-    rw [ofNat_fin_two]
-    norm_num
-    simp [XGate, Oracle, PlusQbit, Hadamard, mul_apply]
-    rw [ofNat_fin_two]
-    norm_num
+-- theorem DeutschNew (a b : Bool) : Hadamard 1 * (Oracle a b) * Hadamard 1 = 2 * (-1)^(a.toNat) * XGate^(a.toNat + b.toNat) :=
+--   by
+--     cases a; cases b;
+--     simp [XGate, Oracle, PlusQbit, Hadamard 1, mul_apply]
+--     rw [ofNat_fin_two]
+--     norm_num
+--     simp [XGate, Oracle, PlusQbit, Hadamard 1, mul_apply]
+--     rw [ofNat_fin_two]
+--     norm_num
+--     cases b;
+--     simp [XGate, Oracle, PlusQbit, Hadamard 1, mul_apply]
+--     rw [ofNat_fin_two]
+--     norm_num
+--     simp [XGate, Oracle, PlusQbit, Hadamard 1, mul_apply]
+--     rw [ofNat_fin_two]
+--     norm_num
 
 
 -- Create a theorem with inputs a and b which are either 0 or 1 (Natural number type)
