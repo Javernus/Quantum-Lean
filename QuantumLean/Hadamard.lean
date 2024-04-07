efbf2814e8aa89e9ed8ea5f24efa1f09474c28ab
Jake Jongejans <jake.jongejans@enreach.com>
2024-04-07 13:48:05 +0200

    feat: Simplification on Hadamard code.
    Co-authored-by: Mitchell Lee <https://github.com/trivial1711>

--- QuantumLean/Hadamard.lean
+++ QuantumLean/Hadamard.lean
@@ -6,97 +6,49 @@ open Matrix
 open Kronecker
 
 
-section HnxHn
+section Hadamard
 variable { n : ℕ }
 
-@[simp]
 def reindexHadamard (A : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ) : Matrix (Fin (2 ^ (n + 1))) (Fin (2 ^ (n + 1))) ℤ :=
   reindex finProdFinEquiv finProdFinEquiv A
 
 
-@[simp]
-theorem reindexHadamard_smul (A : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ) (c : ℕ) : reindexHadamard (c • A) = c • reindexHadamard A := by
-  simp only [reindexHadamard, Nat.add_eq, Nat.add_zero, Nat.pow_eq, reindex_apply]
-  rw [submatrix_smul]
-  simp
-
-@[simp]
-theorem reindexHadamard_one : reindexHadamard (1 : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ) = 1 := by
-  simp
+theorem reindexHadamard_mul (A B : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ) : reindexHadamard (A * B) = reindexHadamard A * reindexHadamard B :=
+  Matrix.submatrix_mul _ _ _ _ _ (finProdFinEquiv.symm.bijective)
 
 
-@[simp]
-theorem reindexHadamard_smul_one (c : ℕ) : reindexHadamard (c • (1 : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ)) = c • 1 := by
-  rw [reindexHadamard_smul, reindexHadamard_one]
+theorem reindexHadamard_natCast (m : ℕ) :
+    reindexHadamard (m : Matrix (Fin (2 ^ n) × Fin 2) (Fin (2 ^ n) × Fin 2) ℤ) = m := by
+  ext i j
+  rw [reindexHadamard, reindex_apply, submatrix_apply]
+  simp_rw [← diagonal_natCast, diagonal_apply]
+  simp_rw [finProdFinEquiv.symm.injective.eq_iff]
 
 
-@[simp]
+theorem kronecker_natCast_natCast {A B : Type} [DecidableEq A] [DecidableEq B] (m₁ m₂ : ℕ) :
+    (m₁ : Matrix A A ℤ) ⊗ₖ (m₂ : Matrix B B ℤ) = ((m₁ * m₂ : ℕ) : Matrix (A × B) (A × B) ℤ) := by
+  simp_rw [← nsmul_one]
+  simp only [nsmul_eq_smul_cast ℕ, Nat.cast_id, Nat.cast_mul]
+  rw [smul_kronecker, kronecker_smul, one_kronecker_one, smul_smul]
+  rfl
+
+
 def Hadamard : (n : ℕ) -> Matrix (Fin (2 ^ n)) (Fin (2 ^ n)) ℤ
   | 0 => 1
   | (n + 1) => reindexHadamard (Hadamard n ⊗ₖ !![1, 1; 1, -1])
 
-@[simp]
-theorem HeqH : Hadamard 1 = !![1, 1; 1, -1] := by decide
-
-
-@[simp]
-theorem HxH : Hadamard 1 * Hadamard 1 = 2 := by decide
-
-
-@[simp]
-theorem TwoXI : (2 : Matrix (Fin 2) (Fin 2) ℤ) = (2 : ℕ) • 1 := by simp
-
-
-@[simp]
-theorem TwoToNXI : (2 ^ n : Matrix (Fin (2 ^ n)) (Fin (2 ^ n)) ℤ) = (2 ^ n : ℕ) • 1 := by simp
-
-
-@[simp]
-theorem TwoToNPlus1XI : (2 ^ (n + 1) : Matrix (Fin (2 ^ (n + 1))) (Fin (2 ^ (n + 1))) ℤ) = (2 ^ (n + 1) : ℕ) • 1 := by
-  simp only [smul_eq_mul, mul_comm, pow_succ]
-  rw [mul_comm]
-  simp
-
-
-@[simp]
-theorem TwoXTwoToNXI : (2 : ℕ) • (2 ^ n : ℕ) = 2 ^ (n + 1) := by
-  rw [smul_eq_mul, mul_comm, ← pow_succ]
-
-
-@[simp]
-theorem Hsuccn : Hadamard (n + 1) = reindexHadamard (Hadamard n ⊗ₖ !![1, 1; 1, -1]) := by rfl
-
-
-
-@[simp]
-theorem postKroneckerReindex : reindexHadamard (M ⊗ₖ !![1, 1; 1, -1]) * reindexHadamard (M ⊗ₖ !![1, 1; 1, -1]) = reindexHadamard ((M ⊗ₖ !![1, 1; 1, -1]) * (M ⊗ₖ !![1, 1; 1, -1])) := by
-  simp
-
 
 -- In general, I am trying to prove H * H = I (× a constant) for Hadamard matrices
-theorem HnxHn : Hadamard n * Hadamard n = 2 ^ n * 1 := by
+theorem HnxHn : Hadamard n * Hadamard n = (2 ^ n : ℕ) := by
   induction n with
-    | zero => simp
+    | zero => simp [Hadamard]
     | succ n ih =>
-      rw [Hsuccn]
-      simp only [Nat.succ_eq_add_one]
-      -- Turn current (Hn ⊗ₖ H1) * (Hn ⊗ₖ H1) into (Hn * Hn) ⊗ₖ (H1 * H1)
-      rw [postKroneckerReindex, ← mul_kronecker_mul, ← HeqH, HxH, ih]
-      simp only [mul_one]
-      rw [TwoXI, TwoToNXI, kroneckerMap_smul_right _ (2 : ℕ) _ (2 ^ n • 1) 1, kroneckerMap_smul_left _ (2 ^ n) _ _ 1]
-      rw [kroneckerMap_one_one]
-      -- Why do I now have 4 cases to prove? I thought I was only rewriting equivalences...
-      case succ.hf₁ => simp
-      case succ.hf₂ => simp
-      case succ.hf₃ => simp
-      case succ =>
-        -- I don't really know how to solve this anymore, in my head, this is just an equality
-        -- but with the reindexHadamard and powers, I am struggling.
-        rw [← smul_assoc, TwoXTwoToNXI, TwoToNPlus1XI, reindexHadamard_smul_one]
-      simp [nsmul_eq_mul, Nat.cast_pow, Nat.cast_ofNat, mul_assoc]
-      simp [nsmul_eq_mul, mul_comm, mul_assoc]
+      rw [Hadamard, ← reindexHadamard_mul, ← mul_kronecker_mul, ih]
+      rw [show (!![1, 1; 1, -1]) * (!![1, 1; 1, -1]) = ((2 : ℕ) : Matrix (Fin 2) (Fin 2) ℤ) by decide]
+      rw [kronecker_natCast_natCast, reindexHadamard_natCast]
+      rfl
 
-end HnxHn
+end Hadamard
 
 
 -- Mathlib Data Finset Prod
