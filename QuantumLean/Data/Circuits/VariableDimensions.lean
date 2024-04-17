7920b05d2082be1e9636897b17bf09daff8bc13e
Jake Jongejans <jake.jongejans@gmail.com>
2024-04-17 16:05:21 +0200

    feat(variable-dimensions): Proofs more general pow_kronecker_mul theorem.

--- QuantumLean/Data/Circuits/VariableDimensions.lean
+++ QuantumLean/Data/Circuits/VariableDimensions.lean
@@ -37,3 +37,21 @@ theorem pow_kronecker_mul { n : ℕ } (M N : nMatrix 1) : pow_kronecker n (M * N
       rw [← reindex_mul]
       rw [← mul_kronecker_mul]
       rw [ih]
+
+
+def stack_gates (n : ℕ) (gates : ℕ -> nMatrix 1) : nMatrix n :=
+  match n with
+    | 0 => 1
+    | (n + 1) => reindex (stack_gates n gates ⊗ₖ gates n)
+
+
+theorem pow_kronecker_mul' { n : ℕ } (M N : ℕ -> nMatrix 1) : stack_gates n M * stack_gates n N = stack_gates (n) (λ i => M i * N i) := by
+  induction n with
+    | zero => simp [stack_gates]
+    | succ m ih =>
+      rw [stack_gates]
+      rw [stack_gates]
+      rw [stack_gates]
+      rw [← reindex_mul]
+      rw [← mul_kronecker_mul]
+      rw [ih]
